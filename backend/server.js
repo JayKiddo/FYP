c18eb9b669609e9fe2525f660d5befaf3e802d69
JayKiddo <ducp1605@gmail.com>
2020-02-21 16:10:20 +0000

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,36 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 
 //Morgan is basically a logger, on any requests being made,it generates logs automatically.
 
 
-//application
-//invoke express application
+//application = instance of express()
 const application = express()
 
-//middlewares
-//HTTP request logger
+
+
+//connect to database
+mongoose.connect(process.env.database_cloud, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false,useUnifiedTopology: true})
+	.then(() => console.log('DB connected'));
+
 //What are the use of these packages ?
 application.use(morgan('dev'));
 application.use(bodyParser.json());
 application.use(cookieParser());
 
-application.use(cors())
+
+//cors error of browser-to-broser communication 
+if(process.env.NODE_ENV === 'development') {
+	application.use(cors({origin: `${process.env.CLIENT_URL}` }));
+}
+
 
 //creating routes
-//Anytime server receives request to slice API,the server respond with current time in json format
+//Server receives request to slice API,the server respond
 application.get('/api',(req,res)=>{
     res.json({time: Date().toString()});
 })
